1bc6d9797cb693b2660860de9212cc8001f54788
leegong <lgustb@gmail.com>
2020-05-12 20:49:31 -0400

    1 commit

---
+++ class2.py
@@ -0,0 +1,11 @@
+class Person:
+  def __init__(self, name, age, sex):
+    self.name = name
+    self.age = age
+    self.sex = sex
+
+p1 = Person('Mike', 36, 'male')
+
+print(p1.name)
+print(p1.age)
+print(p1.sex)
