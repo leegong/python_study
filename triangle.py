d2838cc2df891537358b96910cc214920c160ce8
Ubuntu <lgustb@gmail.com>
2020-09-26 02:18:49 +0000

    Add existing file

---
+++ triangle.py
@@ -0,0 +1,13 @@
+
+import math
+
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+if a + b > c and a + c > b and b + c > a:
+    print('perimeter: %f' % (a + b + c))
+    p = (a + b + c) / 2
+    area = math.sqrt(p * (p - a) * (p - b) * (p - c))
+    print('area: %f' % (area))
+else:
+    print('is not a triangle')
